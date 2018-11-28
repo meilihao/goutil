11afba7c5b918267f2a620dfe15b00f4b3bfd8bb
meilihao <563278383@qq.com>
2018-11-28 16:11:22 +0800

    + time_xb = time_x + blake2b

---
+++ time/time_xb.go
@@ -0,0 +1,88 @@
+package time
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"hash"
+	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/crypto/blake2b"
+)
+
+// use blake2b
+type TimeXB struct {
+	key []byte
+	h       func([]byte) (hash.Hash, error)
+	diff    int64
+	trim2Len int // 截留长度
+}
+
+// size = len(key), size: 32, 48, 64
+func NewTimeXB(size int, key string, diff int64, trimLen int) (*TimeXB, error) {
+	var h func([]byte) (hash.Hash, error)
+
+	switch size {
+	case blake2b.Size:
+		size = blake2b.Size
+		h = blake2b.New512
+	case blake2b.Size384:
+		size = blake2b.Size384
+		h = blake2b.New384
+	default:
+		size = blake2b.Size256
+		h = blake2b.New256
+	}
+
+	if len(key) != size {
+		return nil, errors.New("blake2b: size need equal len(key)")
+	}
+
+	return &TimeXB{
+		key:[]byte(key),
+		h:       h,
+		diff:    diff,
+		trim2Len: trimLen,
+	}, nil
+}
+
+func (x *TimeXB) Generate(s string) string {
+	return x.generate(s, time.Now().Unix())
+}
+
+func (x *TimeXB) generate(s string, now int64) string {
+	tmp := fmt.Sprintf("%s-%d", s, now)
+
+	h,_:= x.h(x.key)
+	h.Write([]byte(tmp))
+
+	result:=hex.EncodeToString(h.Sum(nil))
+	if x.trim2Len>0{
+		result=result[:x.trim2Len]
+	}
+
+	return tmp + "-" +result
+}
+
+func (x *TimeXB) Parse(s string) (string, error) {
+	tmp := strings.Split(s, "-")
+
+	if len(tmp) != 3 {
+		return "", errors.New("no TimeXB")
+	}
+
+	now, _ := strconv.ParseInt(tmp[1], 10, 64)
+
+	if s != x.generate(tmp[0], now) {
+		return "", errors.New("invalid TimeXB")
+	}
+
+	current := time.Now().Unix()
+	if !(now > 0 && (current-now <= x.diff || now-current <= x.diff)) {
+		return "", errors.New("invalid timestamp")
+	}
+
+	return tmp[0], nil
+}
---
+++ time/time_xb_test.go
@@ -0,0 +1,45 @@
+package time
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+func TestTimeXBTrim10(t *testing.T) {
+	x, err := NewTimeXB(32, "hxYYu2Gka5BGqym17C12id8rXOPf1vVI", 2, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	src := "ABC"
+
+	out := x.Generate(src)
+	fmt.Println(out)
+	tmp, err := x.Parse(out)
+	if err != nil {
+		t.Errorf("TimeXB.Parse failed: %v", err)
+	}
+	if tmp != src {
+		t.Errorf("TimeXB.Parse failed: not equal src(%s != %s)", src, tmp)
+	}
+}
+
+func TestTimeXBNoTrim(t *testing.T) {
+	x, err := NewTimeXB(32, "hxYYu2Gka5BGqym17C12id8rXOPf1vVI", 2, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	src := "ABC"
+
+	out := x.Generate(src)
+	fmt.Println(out)
+	tmp, err := x.Parse(out)
+	if err != nil {
+		t.Errorf("TimeXB.Parse failed: %v", err)
+	}
+	if tmp != src {
+		t.Errorf("TimeXB.Parse failed: not equal src(%s != %s)", src, tmp)
+	}
+}
