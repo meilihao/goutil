73a4e2c609d3df2155a02093867400093533dcc9
meilihao <563278383@qq.com>
2018-12-24 12:07:05 +0800

    + loc

--- db/mysql.go
+++ db/mysql.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"net/url"
 	"time"
 
 	// to prevent: "Register called twice for driver mysql",change init to main.go
@@ -15,20 +16,32 @@ type MySQLConfig struct {
 	Name         string
 	Username     string
 	Password     string
-	MaxIdleConns int  `yaml:"max_idle_conns"`
-	MaxOpenConns int  `yaml:"max_open_conns"`
-	ShowSQL      bool `yaml:"show_sql"`
+	MaxIdleConns int    `yaml:"max_idle_conns"`
+	MaxOpenConns int    `yaml:"max_open_conns"`
+	ShowSQL      bool   `yaml:"show_sql"`
+	Loc          string `yaml:"loc"`
 }
 
 func InitMySQL2Xorm(conf *MySQLConfig) (*xorm.Engine, error) {
+	if conf.Loc == "" {
+		conf.Loc = url.QueryEscape("Asia/Shanghai")
+	}
+
+	fmt.Println(*conf)
 	engine, err := xorm.NewEngine(conf.DriverName,
-		fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&loc=Asia%%2FShanghai",
+		fmt.Sprintf(`%s:%s@tcp(%s)/%s?parseTime=true&loc=%s`,
 			conf.Username,
 			conf.Password,
 			conf.Addr,
-			conf.Name))
+			conf.Name,
+			conf.Loc))
 	if err != nil {
 		return nil, err
+	}
+
+	if conf.Loc == "UTC" {
+		engine.DatabaseTZ = time.UTC
+		engine.TZLocation = time.UTC
 	}
 
 	engine.SetMaxOpenConns(conf.MaxOpenConns)
