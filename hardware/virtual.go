b909d678cee41cf90987721930c26e7ccfc8bfea
meilihao <563278383@qq.com>
2020-01-15 17:42:37 +0800

    + 虚拟化判断

---
+++ hardware/virtual.go
@@ -0,0 +1,40 @@
+package hardware
+
+import (
+	"bytes"
+	"errors"
+	"os"
+	"os/exec"
+)
+
+var (
+	ErrRunWithoutRoot = errors.New("权限必须以root运行")
+	ErrNoVirtWhat     = errors.New("未找到virt-what命令")
+)
+
+func VirtualInfo() (string, error) {
+	if os.Geteuid() != 0 {
+		return "", ErrRunWithoutRoot
+	}
+
+	cmdPath := "/usr/sbin/virt-what"
+	if !FileIsExist(cmdPath) {
+		return "", ErrNoVirtWhat
+	}
+
+	cmd := exec.Command("cmdPath")
+	data, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes.TrimSpace(data)), nil
+}
+
+func FileIsExist(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
